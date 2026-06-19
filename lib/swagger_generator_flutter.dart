0ed7f96bbec5757ea35ab0a3a15de4366d9cc6eb
Verilyzed <c.sndr2204@gmail.com>
2026-06-20 01:12:03 +0200

    Convert to pure Dart generator package with build deps

--- lib/swagger_generator_flutter.dart
+++ lib/swagger_generator_flutter.dart
@@ -1,5 +1,2 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+/// Generates Dart code from OpenAPI (Swagger) specifications.
+library;
---
+++ test/setup_test.dart
@@ -0,0 +1,7 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('test harness runs', () {
+    expect(1 + 1, 2);
+  });
+}
