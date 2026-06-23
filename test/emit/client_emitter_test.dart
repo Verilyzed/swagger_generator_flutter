d0575ae7430afa64ccb57c73343e22457af30287
Verilyzed <c.sndr2204@gmail.com>
2026-06-23 10:24:16 +0200

    Make converter typedef public and rename generic to avoid Item clash

--- test/emit/client_emitter_test.dart
+++ test/emit/client_emitter_test.dart
@@ -23,7 +23,7 @@ void main() {
     );
     expect(
       out,
-      contains('Future<Response<ResultType>> convertResponse<ResultType, Item>('),
+      contains('Future<Response<ResultType>> convertResponse<ResultType, Inner>('),
     );
     expect(out, contains('converter: const JsonSerializableConverter({'));
     expect(out, contains('Gadget: Gadget.fromJson,'));
